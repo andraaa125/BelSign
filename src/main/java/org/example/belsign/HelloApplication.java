11500e6c89a23cd061764dae676255f325c46ee1
HaliimoAadan <haliimasheikh@hotmail.com>
2025-04-25 12:53:08 +0200

    conflictss

--- src/main/java/org/example/belsign/HelloApplication.java
+++ src/main/java/org/example/belsign/HelloApplication.java
@@ -17,6 +17,7 @@ public class HelloApplication extends Application {
         stage.show();
     }
 // hello from Haliimo
+    //my super cool code
     public static void main(String[] args) {
         launch();
     }
