91e496d9d270a439dd8c667dab53b63a91a394ca
HaliimoAadan <haliimasheikh@hotmail.com>
2025-04-25 12:29:50 +0200

    comment

--- src/main/java/org/example/belsign/HelloApplication.java
+++ src/main/java/org/example/belsign/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-
+// hello from Andra
     public static void main(String[] args) {
         launch();
     }
