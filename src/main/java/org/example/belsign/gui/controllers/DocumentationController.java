429690a0b82a04ae5075671758d61142377e4004
HaliimoAadan <haliimasheikh@hotmail.com>
2025-05-01 16:09:20 +0200

    PhotoDocumentationView.fxml

---
+++ src/main/java/org/example/belsign/gui/controllers/DocumentationController.java
@@ -0,0 +1,11 @@
+package org.example.belsign.gui.controllers;
+
+import javafx.event.ActionEvent;
+
+public class DocumentationController {
+    public void onClickSend(ActionEvent actionEvent) {
+    }
+
+    public void onClickCancel(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/org/example/belsign/gui/controllers/OperatorDashboardController.java
+++ src/main/java/org/example/belsign/gui/controllers/OperatorDashboardController.java
@@ -1,4 +1,32 @@
 package org.example.belsign.gui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class OperatorDashboardController {
+
+    @FXML
+    private Button btnDocument;
+
+
+    public void onClickDocument(ActionEvent actionEvent) throws IOException {
+        loadDocumentView();
+    }
+
+    private void loadDocumentView() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/org/example/belsign/DocumentationView.fxml"));
+        System.out.println(getClass().getResource("OperatorDashboard.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = new Stage();
+        stage.setTitle("Photo Documentation");
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
