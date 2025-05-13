819264171bdc52761c2b0892041bd5bb66745a04
Andra <andradanielevici1@gmail.com>
2025-05-14 00:07:17 +0200

    just a backup branch for testing

--- src/main/java/org/example/belsign/Main.java
+++ src/main/java/org/example/belsign/Main.java
@@ -16,7 +16,9 @@ public class Main extends Application {
         stage.setScene(scene);
         stage.show();
     }
-  
+
+
+
     public static void main(String[] args) {
         launch();
     }
