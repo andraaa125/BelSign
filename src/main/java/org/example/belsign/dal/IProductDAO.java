701da753c71ada363995fd3f33d8e54b1d68c4e5
HaliimoAadan <haliimasheikh@hotmail.com>
2025-05-14 21:58:36 +0200

    OperatorDashboard.fxml (numbers) & Product classes

---
+++ src/main/java/org/example/belsign/dal/IProductDAO.java
@@ -0,0 +1,13 @@
+package org.example.belsign.dal;
+
+import org.example.belsign.be.Product;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IProductDAO {
+    List<Product> getAllProduct() throws SQLException, IOException;
+
+    void updateProductImage(Product product, String slotType, String imagePath);
+}
